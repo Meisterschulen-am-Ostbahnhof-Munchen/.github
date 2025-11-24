3a7e0e0aed368090b246da9892e7f4cd91ac4fe5
franz-ms-muc <69573151+franz-ms-muc@users.noreply.github.com>
2025-11-24 13:29:02 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,6 @@ project = "Wiki der Meisterschulen am Ostbahnhof München (0)"
 copyright = "2022-2025, Meisterschulen am Ostbahnhof - München"
 author = "Franz Höpfinger"
 
-html_baseurl = "https://docs.ms-muc-docs.de"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -54,3 +53,4 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
